89128979f9a27c1a857d95c1295bbabdfaf85e6f
dimasdevspro <dimasdevspro@github.com>
2020-10-22 13:20:52 -0300

    update server with html

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".card");
+
+for (let card of cards) {
+  card.addEventListener("click", function () {
+    const imgId = card.getAttribute("id");
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector("img").src = `${imgId}`;
+
+    const title = card.querySelector(".title_revenue p");
+    modalOverlay.querySelector(".modal-content .title_revenue p").textContent =
+      title.textContent;
+
+    const author = card.querySelector(".author_revenue p");
+    modalOverlay.querySelector(".modal-content .author_revenue p").textContent =
+      author.textContent;
+  });
+}
+
+document.querySelector(".close-modal").addEventListener("click", function () {
+  modalOverlay.classList.remove("active");
+  modalOverlay.querySelector("iframe").src = "";
+});
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const express = require('express');
+const express = require("express");
+const nunjucks = require("nunjucks");
+const revenues = require("./data");
 
-const server = express()
+const server = express();
 
-server.get("/", function(req, res){
-   return res.send("Hi! Estou funcionando!")
-})
+server.use(express.static("public"));
+server.use(express.static("assets"));
 
-server.listen(3333, function(){
-    console.log('Server is running!')
-})
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+  express: server,
+});
+
+server.get("/", function (req, res) {
+  return res.render("index");
+});
+
+server.get("/about.html", function (req, res) {
+  return res.render("about");
+});
+
+server.get("/revenue.html", function (req, res) {
+  return res.render("revenue");
+});
+
+server.use(function (req, res) {
+  return res.status(404).render("not-found");
+});
+
+server.listen(3333, function () {
+  console.log("Server is running!");
+});
