cd73abd0e4b1816c16a6554be22bacd6cefefedc
dimasdevspro <dimasdevspro@github.com>
2020-10-26 16:06:57 -0300

    button showorhide finished and more details at pages

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ module.exports = [
         '3 colheres (sopa) de tempero caseiro: feito com alho, sal, cebola, pimenta e cheiro verde processados no liquidificador',
         '30 ml de água gelada'
       ],
-      preparation: [
+      preparations: [
         'Misture todos os ingredientes muito bem e amasse para que fique tudo muito bem misturado.',
         'Faça porções de 90 g a 100 g.',
         'Forre um plástico molhado em uma bancada e modele os hambúrgueres utilizando um aro como base.',
@@ -20,7 +20,7 @@ module.exports = [
         'Faça no máximo 4 camadas por forma e leve para congelar.',
         'Retire do congelador, frite ou asse e está pronto.'
       ],
-      information: "Preaqueça a chapa, frigideira ou grelha por 10 minutos antes de levar os hambúrgueres. Adicione um pouquinho de óleo ou manteiga e não amasse os hambúrgueres! \n\n Você sabia que a receita que precede o hambúrguer surgiu no século XIII, na Europa? A ideia de moer a carne chegou em Hamburgo no século XVII, onde um açogueiro resolveu também temperá-la. Assim, a receita foi disseminada nos Estados Unidos por alemães da região. Lá surgiu a ideia de colocar o hambúrguer no meio do pão e adicionar outros ingredientes, como queijom tomates e alface."
+      informations: "Preaqueça a chapa, frigideira ou grelha por 10 minutos antes de levar os hambúrgueres. Adicione um pouquinho de óleo ou manteiga e não amasse os hambúrgueres! \n\n Você sabia que a receita que precede o hambúrguer surgiu no século XIII, na Europa? A ideia de moer a carne chegou em Hamburgo no século XVII, onde um açogueiro resolveu também temperá-la. Assim, a receita foi disseminada nos Estados Unidos por alemães da região. Lá surgiu a ideia de colocar o hambúrguer no meio do pão e adicionar outros ingredientes, como queijom tomates e alface."
     },
     {
       image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/pizza.png',
@@ -40,14 +40,14 @@ module.exports = [
         'azeitona picada',
         'orégano a gosto'
       ],
-      preparation: [
+      preparations: [
         'No liquidificador bata o leite, o ovo, o sal, o açúcar, a margarina, a farinha de trigo e o fermento em pó até que tudo esteja encorporado.',
         'Despeje a massa em uma assadeira para pizza untada com margarina e leve ao forno preaquecido por 20 minutos.',
         'Retire do forno e despeje o molho de tomate.',
         'Cubra a massa com mussarela ralada, tomate e orégano a gosto.',
         'Leve novamente ao forno até derreter a mussarela.'
       ],
-      information: 'Pizza de liquidificador é uma receita deliciosa e supersimples de preparar. Feita toda no liquidificador, ela é bem prática para o dia a dia. Aqui no TudoGostoso você também encontra diversas delícias práticas feitas no liquidificador: massa de panqueca, torta de frango de liquidificador, pão de queijo de liquidificador, bolo de banana, bolo de chocolate e muito mais!'
+      informations: 'Pizza de liquidificador é uma receita deliciosa e supersimples de preparar. Feita toda no liquidificador, ela é bem prática para o dia a dia. Aqui no TudoGostoso você também encontra diversas delícias práticas feitas no liquidificador: massa de panqueca, torta de frango de liquidificador, pão de queijo de liquidificador, bolo de banana, bolo de chocolate e muito mais!'
     },
     {
       image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/asinha.png',
@@ -59,12 +59,12 @@ module.exports = [
         '1/2 xícara (chá) de óleo',
         '1 xícara de molho barbecue'
       ],
-      preparation: [
+      preparations: [
         'Em uma tigela coloque o encontro de asinha de galinha e polvilhe a farinha de trigo e misture com as mãos.',
         'Em uma frigideira ou assador coloque o óleo quando estiver quente frite até ficarem douradas.',
         'Para servir fica bonito com salada, ou abuse da criatividade.'
       ],
-      information: ''
+      informations: ''
     },
     {
       image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/lasanha.png',
@@ -77,14 +77,14 @@ module.exports = [
         '2 copos de requeijão',
         '150 g de mussarela para gratinar'
       ],
-      preparation: [
+      preparations: [
         'Em uma panela, coloque a manteiga para derreter.',
         'Acrescente a farinha de trigo e misture bem com auxílio de um fouet.',
         'Adicione o leite e misture até formar um creme homogêneo.',
         'Tempere com sal, pimenta e noz-moscada a gosto.',
         'Desligue o fogo e acrescente o creme de leite; misture bem e reserve.'
       ],
-      information: 'Recheie a lasanha com o que preferir.'
+      informations: 'Recheie a lasanha com o que preferir.'
     },
     {
       image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/macarrao.png',
@@ -101,14 +101,14 @@ module.exports = [
         'gengibre em pó a gosto',
         '1 folha de louro'
       ],
-      preparation: [
+      preparations: [
         'Quando faltar mais ou menos 5 minutos para ficar no ponto de escorrer o macarrão, comece o preparo da receita.',
         'Na frigideira quente coloque a manteiga, o azeite, a folha de louro, e o alho granulado.',
         'Nesta hora um pouco de agilidade, pois o macarrão escorrido vai para a frigideira, sendo mexido e dosado com sal a gosto, as ervas, o gengibre em pó a gosto também.',
         'O dente de alho, serve para você untar os pratos onde serão servidos o macarrão.',
         'Coloque as porções nos pratos já com o cheiro do alho, e enfeite com algumas ervas.'
       ],
-      information: 'Não lave o macarrão nem passe óleo ou gordura nele depois de escorrê-lo. Coloque direto na frigideira.'
+      informations: 'Não lave o macarrão nem passe óleo ou gordura nele depois de escorrê-lo. Coloque direto na frigideira.'
     },
     {
       image: 'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/launchbase/receitas/doce.png',
@@ -124,12 +124,12 @@ module.exports = [
         'manteiga para untar',
         'açúcar de confeiteiro'
       ],
-      preparation: [
+      preparations: [
         'Cozinhe a batata-doce numa panela de pressão, com meio litro de água, por cerca de 20 minutos. Descasque e passe pelo espremedor, ainda quente.',
         'Junte a manteiga,os ovos, o coco ralado,o açúcar, o Leite Moça e o fermento em pó, mexendo bem após cada adição.',
         'Despeje em assadeira retangular média, untada e leve ao forno médio (180°C), por aproximadamente 45 minutos. Depois de frio, polvilhe, com o',
         'açúcar de confeiteiro e corte em quadrados.'
       ],
-      information: ''
+      informations: ''
     }
   ];
